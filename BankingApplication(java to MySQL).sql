1361ce34a64ff3e3c977a6131412bc9291498ee6
tarun <tarunbk1947@gmail.com>
2026-07-09 19:02:27 +0530

    Initial commit

---
+++ BankingApplication(java to MySQL).sql
@@ -0,0 +1,11 @@
+create database bankingApp_db;
+use bankingApp_db;
+
+create table banking_info(
+   user_Id int,
+   user_Name varchar(50),
+   user_Email varchar(50),
+   user_Password varchar(50),
+   address varchar(50));
+   
+select * from banking_info;
