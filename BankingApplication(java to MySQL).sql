13e91ee49be4d9b32cb9786dc090ed1d2d53d181
Tarunbk648 <tarunbk1947@gmail.com>
2026-07-13 13:48:18 +0530

    Modify banking_info and create manager_info table
    Added primary key constraint to banking_info and created manager_info table with foreign key reference.

--- BankingApplication(java to MySQL).sql
+++ BankingApplication(java to MySQL).sql
@@ -9,3 +9,22 @@ create table banking_info(
    address varchar(50));
    
 select * from banking_info;
+
+
+desc banking_info;
+
+ALTER TABLE banking_info
+ADD CONSTRAINT PK_banking_info PRIMARY KEY (user_Id);   -- add/modify a primary key to the user Id
+
+
+-- Performing a join operation
+create table manager_info(
+      manager_Id int,
+      user_Id int,
+	  manager_Name varchar(50),
+      manager_Email varchar(50),
+      manager_Password varchar(50),
+      address varchar(50),
+      foreign key(user_Id) references banking_info(user_Id));
+      
+select * from manager_info;
